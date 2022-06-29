f304545a5cd7c33e19b25b404991320d890ca7d7
maicolortz <maicolortiz28971@gmail.com>
2022-06-28 22:26:45 -0500

    html resuelto

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,25 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+   function Usuario(opciones){
+    this.usuario=opciones.usuario;
+    this.nombre=opciones.nombre;
+    this.email=opciones.email;
+    this.password=opciones.password;
+  } 
+    Usuario.prototype.saludar=function(){
+      return "Hola, mi nombre es "+ this.nombre
+    }
+    return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar=function() {
+    return "Hello World!";
+  } 
 }
 
 function agregarStringInvertida() {
@@ -46,11 +59,16 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  this.nombre="Juan";
+  this.apellido="Perez";
+  this.edad=22;
+  this.dir="Saavedra 123";
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  
 }
   
 
